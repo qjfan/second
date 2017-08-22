31d200181326685408e5d8fd66603b44f1962468
qjfan <zziabaike2011@126.com>
2017-08-22 10:08:12 +0800

    bean包，User类

--- src/com/qisheng/Test.java
+++ src/com/qisheng/Test.java
@@ -7,7 +7,7 @@ public class Test {
 
     int x = 29;
 
-    public int add(int x,int y) {
+    public static int add(int x,int y) {
 
         return x + y;
     }
@@ -15,6 +15,8 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("hello");
+
+        add(2,9);
 
 
 
---
+++ src/com/qisheng/bean/User.java
@@ -0,0 +1,28 @@
+package com.qisheng.bean;
+
+/**
+ * Created by Administrator on 2017-08-22.
+ */
+public class User {
+
+    private int id;
+
+    private String username;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
